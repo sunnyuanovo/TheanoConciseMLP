3a9d6c9b1d6aa760c9a7a94fb025072355402a71
Wei <sunnyuanovo@hotmail.com>
2015-09-02 22:00:37 -0400

    add the main function. still column-based

--- main.py
+++ main.py
@@ -165,71 +165,6 @@ def gradient_updates_momentum(cost, params, learning_rate, momentum):
         updates.append((param_update, momentum*param_update + (1. - momentum)*T.grad(cost, param)))
     return updates
 
-# Training data - two randomly-generated Gaussian-distributed clouds of points in 2d space
-np.random.seed(0)
-# Number of points
-N = 10
-# Labels for each cluster
-y = np.random.random_integers(0, 1, N)
-# Mean of each cluster
-means = np.array([[-1, 1], [-1, 1]])
-# Covariance (in X and Y direction) of each cluster
-covariances = (np.random.random_sample((2, 2)) + 1).astype(np.float32)
-
-#print type(covariances), covariances.dtype
-# Dimensions of each point
-X = np.vstack([np.random.randn(N)*covariances[0, y] + means[0, y],
-               np.random.randn(N)*covariances[1, y] + means[1, y]])
-X = X.astype(np.float32)
-y = np.float32(y)
-#print type(X), X.dtype, type(y), y.dtype
-
-
-# First, set the size of each layer (and the number of layers)
-# Input layer size is training data dimensionality (2)
-# Output size is just 1-d: class label - 0 or 1
-# Finally, let the hidden layers be twice the size of the input.
-# If we wanted more layers, we could just add another layer size to this list.
-layer_sizes = [X.shape[0], X.shape[0]*2, 1]
-print layer_sizes
-# Set initial parameter values
-W_init = []
-b_init = []
-activations = []
-for n_input, n_output in zip(layer_sizes[:-1], layer_sizes[1:]):
-    # Getting the correct initialization matters a lot for non-toy problems.
-    # However, here we can just use the following initialization with success:
-    # Normally distribute initial weights
-    W_init.append(np.random.randn(n_output, n_input).astype(np.float32))
-#    print W_init[-1].dtype
-    # Set initial biases to 1
-    b_init.append(np.ones(n_output).astype(np.float32))
-    # We'll use sigmoid activation for all layers
-    # Note that this doesn't make a ton of sense when using squared distance
-    # because the sigmoid function is bounded on [0, 1].
-    activations.append(T.nnet.sigmoid)
-# Create an instance of the MLP class
-mlp = MLP(W_init, b_init, activations)
-
-# Create Theano variables for the MLP input
-mlp_input = T.matrix('mlp_input')
-# ... and the desired output
-mlp_target = T.vector('mlp_target')
-# Learning rate and momentum hyperparameter values
-# Again, for non-toy problems these values can make a big difference
-# as to whether the network (quickly) converges on a good local minimum.
-learning_rate = 0.01
-momentum = 0.9
-# Create a function for computing the cost of the network given an input
-cost = mlp.squared_error(mlp_input, mlp_target)
-# Create a theano function for training the network
-train = theano.function([mlp_input, mlp_target], cost,
-                        updates=gradient_updates_momentum(cost, mlp.params, learning_rate, momentum))
-# Create a theano function for computing the MLP's output given some input
-mlp_output = theano.function([mlp_input], mlp.output(mlp_input))
-
-# Keep track of the number of training iterations performed
-iteration = 0
 # We'll only train the network with 20 iterations.
 # A more common technique is to use a hold-out validation set.
 # When the validation error starts to increase, the network is overfitting,
@@ -238,22 +173,96 @@ iteration = 0
 #print type(X), X.dtype, type(y), y.dtype
 #X.astype(np.float32)
 #print type(X), X.dtype, type(y), y.dtype
-max_iteration = 5
-while iteration < max_iteration:
-    # Train the network using the entire training set.
-    # With large datasets, it's much more common to use stochastic or mini-batch gradient descent
-    # where only a subset (or a single point) of the training set is used at each iteration.
-    # This can also help the network to avoid local minima.
+
+def test_sim():
+    # Training data - two randomly-generated Gaussian-distributed clouds of points in 2d space
+    np.random.seed(0)
+    # Number of points
+    N = 10
+    # Labels for each cluster
+    y = np.random.random_integers(0, 1, N)
+    # Mean of each cluster
+    means = np.array([[-1, 1], [-1, 1]])
+    # Covariance (in X and Y direction) of each cluster
+    covariances = (np.random.random_sample((2, 2)) + 1).astype(np.float32)
+    
+    #print type(covariances), covariances.dtype
+    # Dimensions of each point
+    X = np.vstack([np.random.randn(N)*covariances[0, y] + means[0, y],
+                   np.random.randn(N)*covariances[1, y] + means[1, y]])
+    X = X.astype(np.float32)
+    y = np.float32(y)
+    print type(X), X.shape, type(y), y.shape
     
     
-    current_cost = train(X, y)
-    # Get the current network output for all points in the training set
-    current_output = mlp_output(X)
+    # First, set the size of each layer (and the number of layers)
+    # Input layer size is training data dimensionality (2)
+    # Output size is just 1-d: class label - 0 or 1
+    # Finally, let the hidden layers be twice the size of the input.
+    # If we wanted more layers, we could just add another layer size to this list.
+    layer_sizes = [X.shape[0], X.shape[0]*2, 1]
+    print layer_sizes
+    # Set initial parameter values
+    W_init = []
+    b_init = []
+    activations = []
+    for n_input, n_output in zip(layer_sizes[:-1], layer_sizes[1:]):
+        print "n_input,n_output = %d,%d" % (n_input, n_output)
+        # Getting the correct initialization matters a lot for non-toy problems.
+        # However, here we can just use the following initialization with success:
+        # Normally distribute initial weights
+        W_init.append(np.random.randn(n_output, n_input).astype(np.float32))
+    #    print W_init[-1].dtype
+        # Set initial biases to 1
+        b_init.append(np.ones(n_output).astype(np.float32))
+        # We'll use sigmoid activation for all layers
+        # Note that this doesn't make a ton of sense when using squared distance
+        # because the sigmoid function is bounded on [0, 1].
+        activations.append(T.nnet.sigmoid)
+    # Create an instance of the MLP class
+    mlp = MLP(W_init, b_init, activations)
+    
+    # Create Theano variables for the MLP input
+    mlp_input = T.matrix('mlp_input')
+    # ... and the desired output
+    mlp_target = T.vector('mlp_target')
+    # Learning rate and momentum hyperparameter values
+    # Again, for non-toy problems these values can make a big difference
+    # as to whether the network (quickly) converges on a good local minimum.
+    learning_rate = 0.01
+    momentum = 0.9
+    # Create a function for computing the cost of the network given an input
+    cost = mlp.squared_error(mlp_input, mlp_target)
+    # Create a theano function for training the network
+    train = theano.function([mlp_input, mlp_target], cost,
+                            updates=gradient_updates_momentum(cost, mlp.params, learning_rate, momentum))
+    # Create a theano function for computing the MLP's output given some input
+    mlp_output = theano.function([mlp_input], mlp.output(mlp_input))
+    
+    # Keep track of the number of training iterations performed
+    iteration = 0
     
     
     
-    # We can compute the accuracy by thresholding the output
-    # and computing the proportion of points whose class match the ground truth class.
-    accuracy = np.mean((current_output > .5) == y)
-    print iteration, accuracy
-    iteration += 1
+    max_iteration = 5
+    while iteration < max_iteration:
+        # Train the network using the entire training set.
+        # With large datasets, it's much more common to use stochastic or mini-batch gradient descent
+        # where only a subset (or a single point) of the training set is used at each iteration.
+        # This can also help the network to avoid local minima.
+        
+        
+        current_cost = train(X, y)
+        # Get the current network output for all points in the training set
+        current_output = mlp_output(X)
+        
+        
+        
+        # We can compute the accuracy by thresholding the output
+        # and computing the proportion of points whose class match the ground truth class.
+        accuracy = np.mean((current_output > .5) == y)
+        print iteration, accuracy
+        iteration += 1
+
+if __name__ == '__main__':
+    test_sim()
